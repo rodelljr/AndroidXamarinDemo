fb32b7fc685c6197eef09df84da1c119c7a98448
Roger O'Dell <roger.odell.jr@gmail.com>
2019-11-12 22:31:20 -0600

    Initial Project Construction

---
+++ Android Xamarin Demo/Models/GenericParcelableCreator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace XamarinDemo.Models
+{
+    public class GenericParcelableCreator<T> : Java.Lang.Object,
+        IParcelableCreator where T : Java.Lang.Object, new()
+    {
+        private readonly Func<Parcel, T> createFunc;
+
+        public GenericParcelableCreator(Func<Parcel, T> createFromParcelFunc)
+        {
+            createFunc = createFromParcelFunc;
+        }
+
+        public Java.Lang.Object CreateFromParcel(Parcel source)
+        {
+            return createFunc(source);
+        }
+
+        public Java.Lang.Object[] NewArray(int size)
+        {
+            return new T[size];
+        }
+    }
+}
---
+++ Android Xamarin Demo/Models/ResponseModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Interop;
+
+namespace XamarinDemo.Models
+{
+    public class ResponseModel : Java.Lang.Object, IParcelable
+    {
+        private static readonly GenericParcelableCreator<ResponseModel> creator
+            = new GenericParcelableCreator<ResponseModel>((parcel) => new ResponseModel(parcel));
+
+        public string Message { get; set; }
+        public long Duration { get; set; }
+        public long Risetime { get; set; }
+
+        public ResponseModel() { }
+
+        public ResponseModel(Parcel parcel)
+        {
+            Duration = parcel.ReadLong();
+            Risetime = parcel.ReadLong();
+            Message = parcel.ReadString();
+        }
+
+        public int DescribeContents()
+        {
+            return 0;
+        }
+
+        public void WriteToParcel(Parcel dest, [GeneratedEnum] ParcelableWriteFlags flags)
+        {
+            dest.WriteLong(Duration);
+            dest.WriteLong(Risetime);
+            dest.WriteString(Message);
+        }
+
+        [ExportField("CREATOR")]
+        public static GenericParcelableCreator<ResponseModel> GetCreator()
+        {
+            return creator;
+        }
+
+       
+    }
+}
